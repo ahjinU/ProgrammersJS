9032107028235c55c803ed90adef40d6c2ed3765
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-03-29 23:24:11 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,21 @@
+function isPrime(x){
+    for(let i=2;i<x;i++){
+        if(x%i===0) return false;
+    }
+    return true
+}
+function solution(nums) {
+    
+    let cnt =0;
+    
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            for(let k=j+1;k<nums.length;k++){
+                if(isPrime(nums[i]+nums[j]+nums[k])) cnt++;
+            }
+        }
+    }
+    
+    return cnt;
+    
+}
