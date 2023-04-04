593398afd980384a2509cc8d5069811a7aad1a9b
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-04 23:48:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 24.15 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=javascript) 
 
 ### 성능 요약
 
-메모리: 6.66 MB, 시간: 4.56 ms
+메모리: 46.9 MB, 시간: 24.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -7,3 +7,20 @@ function solution(s)
     }
     return stack.length>0? 0: 1;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include<string>
+// using namespace std;
+
+// int solution(string s)
+// {
+//     vector<char> st;
+    
+//     for(int i=0;i<s.length();i++){
+//         if(!st.empty()&&s[i]==st[st.size()-1]) st.pop_back();
+//         else st.push_back(s[i]);
+//     }
+    
+//     return st.empty()? 1: 0;
+// }
