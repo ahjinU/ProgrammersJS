6c9d4935faf7f9f87c6b96acf73cce01cf52b0a3
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-05 11:20:37 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+
+    while(a!=b){
+        if(a%2) a=a/2+1;
+        else a=a/2;
+        if(b%2) b=b/2+1;
+        else b=b/2;
+        answer++;
+    }
+
+    return answer;
+}
