2462dac6f64f2e6edac787ef796fd6b945fd23a9
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-06 17:24:35 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -7,3 +7,20 @@ function solution(n) {
     }
     return arr[n]
 }
+
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// long long solution(int n) {
+//     long long answer = 0;
+//    vector<int> dq;
+//     dq.push_back(0);
+//     dq.push_back(1);
+//     dq.push_back(2);
+//     for(int i=3;i<=n;i++){
+//         dq.push_back((dq[i-1]+dq[i-2])%1234567);
+//     }
+//     return dq[n];
+// }
