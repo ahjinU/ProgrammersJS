7afa9a1da686c041da6f248b6210487674b58cc5
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-07 14:49:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,31 +1,25 @@
 function solution(s) {
-    let map = {
-        "[":+1,
-        "]":-1,
-        "(":+2,
-        ")":-2,
-        "{":+3,
-        "}":-3
-    }
-    let result=0;
-    s= s.split("")
+    if(s.length%2!==0) return 0
+    let answer=0;
+    s=s.split("")
     for(let i=0;i<s.length;i++){
-        let ch =[];
+        let ch =[]
+        let right=true;
         for(let j=0;j<s.length;j++){
-            ch.push(map[s[j]])
-            if(!ch.includes(-map[s[j]])
-               &&map[s[j+1]]!==-map[s[j]]
-              &&map[s[j+1]]<0) break;
-            if(map[s[j]]<0&&!ch.includes(-map[s[j]])) break;
-            if(ch.reduce((a,c)=> a+c)<0) break;
+            if(s[j]=='('||s[j]=='['||s[j]=='{') ch.push(s[j]);
+            else{
+                let top = ch.pop();
+                if(s[j]==')'&&top=='(') continue;
+                if(s[j]==']'&&top=='[') continue;
+                if(s[j]=='}'&&top=='{') continue;
+                else {
+                    right=false;
+                }
+            }
         }
-        if(ch.reduce((a,c)=> a+c)==0) {
-            result++;
-        }
-        s.push(s[0])
-        s.shift(s[0])
-        //console.log(s,result)
+        if(right) answer++;
+        s.push(s[0]);
+        s.shift(s[0]);
     }
-   
-    return result
+    return answer;
 }
