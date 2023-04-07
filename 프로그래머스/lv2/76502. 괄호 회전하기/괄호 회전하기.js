f6d5bb69bfbdebb9e58607fa1fdece715afaf6c2
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-07 15:13:11 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -23,3 +23,39 @@ function solution(s) {
     }
     return answer;
 }
+/*
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    if(s.length()%2!=0) return 0;
+    int answer=0;
+    for(int i=0;i<s.length();i++){
+        stack<char> st;
+        bool ch = true;
+        for(int j=0;j<s.length();j++){
+            if(s[j]=='('||s[j]=='{'||s[j]=='[') st.push(s[j]);
+            else{
+                char ts = st.top();
+                st.pop();
+                if(s[j]==')'&&ts=='(') continue;
+                if(s[j]==']'&&ts=='[') continue;
+                if(s[j]=='}'&&ts=='{') continue;
+                else {
+                    ch = false;
+                    break;
+                }
+            }
+        }
+        if(ch) answer++;
+        
+        char sfir = s.front();
+        s.erase(s.begin());
+        s.push_back(sfir);
+    }
+    return answer;
+}*/
