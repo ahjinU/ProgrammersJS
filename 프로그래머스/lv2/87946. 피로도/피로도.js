dc4c908029d837bb72ac856390ecd68270f3f654
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-12 23:14:07 +0900

    [level 2] Title: 피로도, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,28 +1,17 @@
-    
-    function allV(ch){
-        for(let i=0;i<ch.length;i++){
-            if(ch[i]===0){
-                return false
-            }
-        }
-        return true
-    }
-
 function solution(k, dungeons) {
-    let max=0;
-    let ch = new Array(dungeons.length).fill(0)
-
-    dfs(0,k)
-    function dfs(num,cnt){
-        max = max<num? num: max;
-        for(let i=0;i<dungeons.length;i++){
-            if(ch[i]===1) continue;
-            if(cnt>=dungeons[i][0]){
-                 ch[i]=1;
-                dfs(num+1,cnt-dungeons[i][1])
-                ch[i]=0;
-            }
-        }
-    }
+    let max =0;
+   let ch = new Array(dungeons.length).fill(0)
+   dfs(0,k)
+   function dfs(num,energy){
+       max = num>max? num: max;
+       for(let i=0;i<dungeons.length;i++){
+           if(ch[i]===1) continue
+           if(energy>=dungeons[i][0]){
+               ch[i]=1;
+               dfs(num+1,energy-dungeons[i][1])
+               ch[i]=0;
+           }
+       }
+   }
     return max
 }
