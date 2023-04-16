72a97a65f7f18dd26a579ea6dada35ea97424848
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-04-16 17:18:23 +0900

    [level 2] Title: 방문 길이, Time: 6.95 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -10,22 +10,15 @@ function solution(dirs) {
     let now0 = 0
     let now1 = 0
     for(let i=0;i<dirs.length;i++){
-        console.log(dirs[i])
+        visit.push(now0.toString()+now1.toString())
         let next0 = now0+four[dirs[i]][0]
         let next1 = now1+four[dirs[i]][1]
         if(Math.abs(next0)>5||Math.abs(next1)>5) continue
         
-        if(visit.includes(now0.toString()+now1.toString()+next0.toString()+next1.toString())||visit.includes(next0.toString()+next1.toString()+now0.toString()+now1.toString())){
-             now0=next0
-            now1=next1
-        } 
-        
-        else visit.push(now0.toString()+now1.toString()+next0.toString()+next1.toString())
-        
-         now0=next0
+        now0=next0
         now1=next1
-        
     }
-    return visit.length
+    let final = new Set(visit)
+    return final.size
   
 }
